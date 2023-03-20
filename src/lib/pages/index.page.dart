c73a520eb844e8442fa16ee7009db866cf9dfef1
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-03-20 01:34:05 -0300

    feature: add home splash page

--- src/lib/pages/index.page.dart
+++ src/lib/pages/index.page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:echo_store/pages/login.page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:echo_store/utils/color.pallete.dart';
@@ -15,13 +16,15 @@ class _IndexPageState extends State<IndexPage> {
   @override
   void initState() {
     super.initState();
-
-    SystemChrome.setEnabledSystemUIMode([SystemUiOverlay.bottom] as SystemUiMode);
+    
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    pushLoginPage();
   }
 
-   void pushHomePage(){
-      Timer(const Duration(seconds: 1), (){
-          NavigationBloc().pushReplacementTo(context, new WelcomePage());
+   void pushLoginPage(){
+      Timer(const Duration(seconds: 2), (){
+          Navigator.pushReplacement(context, 
+            MaterialPageRoute(builder: (BuildContext builder) => const LoginPage()));
       });
   } 
 
--- src/lib/pages/login.page.dart
+++ src/lib/pages/login.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget{
   const LoginPage({super.key});
@@ -8,6 +9,12 @@ class LoginPage extends StatefulWidget{
 }
 
 class _LoginPageState extends State<LoginPage>{
+  @override
+  void initState(){
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
   Widget build(BuildContext context){
     return Scaffold(
       body: Container());}

0f895600c95296b2298069ba972447facdbcc4bb
lucas.oliveira <lucasolrocha90@gmail.com>
2023-04-19 17:16:54 -0300

    feature: add register page

--- src/lib/pages/register.page.dart
+++ src/lib/pages/register.page.dart
@@ -1,5 +1,11 @@
+import 'package:echo_store/pages/login.page.dart';
+import 'package:echo_store/utils/sizes.dart';
+import 'package:echo_store/utils/toasts.dart';
+import 'package:echo_store/widgets/echo.button.widget.dart';
+import 'package:echo_store/widgets/echo.input.widget.dart';
 import 'package:flutter/material.dart';
 
+import '../router/router.dart';
 import '../widgets/echo.page.widget.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -10,8 +16,110 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+
+  final nameInputController = TextEditingController();
+  final emailInputController = TextEditingController();
+  final passwordInputController = TextEditingController();
+
+  @override
+  void dispose() { //garbage collector da shopee
+    nameInputController.dispose();
+    emailInputController.dispose();
+    passwordInputController.dispose();
+    super.dispose();
+  }
+
+  void pushLoginPage() =>
+    EchoRouter.pushReplacement(const LoginPage(), context);
+  
+
+  void registerAsync() async {
+    if(nameInputController.text == "") {
+      Toasts.errorToast("O campo nome não pode estar vazio!");
+    }    
+    
+    if(emailInputController.text == "") {
+      Toasts.errorToast("O campo do seu email não pode estar vazio!");
+    }
+
+    if(passwordInputController.text == "") {
+      Toasts.errorToast("A senha não pode ser vazia!");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: EchoPage(Text("Register Page")));
+    
+    return Scaffold(
+      body: EchoPage(Center( //echo input
+      child:Container( //cada componente pode ou não ter um filho com exceção do column ou row que pode ter vários
+        alignment: Alignment.center,
+        width: Sizes.getPercentWidth(context, 85),
+        height: Sizes.getPercentHeight(context, 55),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top:35),
+            child: EchoInput(controller: nameInputController,
+              icon: Icons.person,
+              borderSide: 2,
+              fontSize: 16,
+              keyboardType: TextInputType.name,
+              hintText: "Digite seu nome completo")),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 35, bottom: 10),
+            child: EchoInput(controller: emailInputController,
+              icon: Icons.mail,
+              borderSide: 2,
+              fontSize: 16,
+              hintText: "Digite seu e-mail")),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 35, bottom: 10),
+            child: EchoInput(
+              controller: passwordInputController,
+              icon: Icons.lock,
+              borderSide: 2,
+              passwordField: true,
+              fontSize: 16,
+              hintText: "Defina sua senha")),
+
+          Center(
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: Sizes.getPercentWidth(context, 2.5)),
+                  child: SizedBox(
+                    width: Sizes.getPercentWidth(context, 40),
+                    height: Sizes.getPercentHeight(context, 3),
+                    child: InkWell(
+                      child: const Text(
+                        "Já possui conta?",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 16,
+                          color: Colors.purple),
+                        ),
+                        onTap: () => pushLoginPage(),                      
+                      )
+                    )
+                  )
+                ]
+              ) //parameters: context - percent
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top:100),
+            child: EchoButton(
+              text: "Finalizar Cadastro",
+              width: Sizes.getPercentWidth(context, 55),
+              height: 45,
+              onPressed: registerAsync)
+            )
+          ]),
+        ),
+      )
+    ));
   }
 }

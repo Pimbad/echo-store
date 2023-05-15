860da919352e7c477795da3ca07ff3d58e1845c7
eschechola <lucas.eschechola@outlook.com>
2023-05-15 13:31:37 -0300

    feature: add getProductById in ProductService

--- src/lib/services/product.service.dart
+++ src/lib/services/product.service.dart
@@ -11,12 +11,22 @@ class ProductService extends BaseService {
     if(response.statusCode == 200){
       final body = json.decode(response.body);
 
-      var products = List<Product>
-        .from(body.map((product) => Product.fromJson(product)));
-
-      return products;
+      return List<Product>.from(body.map((product)
+        => Product.fromJson(product)));
     }
 
     return List.empty();
+  }
+
+  Future<Product?> getProductById(int productId) async {
+    var response = await getAsync("products/$productId");
+
+    if(response.statusCode == 200){
+      final body = json.decode(response.body);
+
+      return Product.fromJson(body);
+    }
+
+    return null;
   }
 }

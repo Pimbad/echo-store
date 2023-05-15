739f93a6b11022fa6e7175ce18ee1d3af2f2906f
eschechola <lucas.eschechola@outlook.com>
2023-05-15 00:44:48 -0300

    feature: add widget to logged header pages

---
+++ src/lib/widgets/echo.home.header.widget.dart
@@ -0,0 +1,65 @@
+import 'package:echo_store/pages/login.page.dart';
+import 'package:echo_store/router/router.dart';
+import 'package:echo_store/utils/color.pallete.dart';
+import 'package:echo_store/utils/sizes.dart';
+import 'package:flutter/material.dart';
+
+class EchoHomeHeader extends StatelessWidget {
+  final StatefulWidget? returnPage;
+  final bool logout;
+
+  const EchoHomeHeader({
+    this.returnPage,
+    this.logout = true,
+    super.key});
+
+  void pushReturnPage(context) =>
+    EchoRouter.pushReplacement(returnPage as StatefulWidget, context);
+
+  void _pushCartPage(context) =>
+    EchoRouter.pushReplacement(const LoginPage(), context);
+
+  void _logout(context){
+    EchoRouter.pushReplacement(const LoginPage(), context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        children: [
+          SizedBox(
+            width: Sizes.getPercentWidth(context, 42.5),
+            child: Padding(
+                padding: const EdgeInsets.only(top: 20,),
+                child: IconButton(
+                  icon: Icon(
+                    logout 
+                    ? Icons.logout
+                    : Icons.arrow_back_sharp,
+                      color: ColorPallete.backgroundColorLight, size: 30),
+                  alignment: Alignment.centerLeft,
+                  onPressed: logout 
+                  ? () => _logout(context)
+                  : () => pushReturnPage(context),
+              )
+            ),
+          ),
+
+          SizedBox(
+              width: Sizes.getPercentWidth(context, 42.5),
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: IconButton(
+                    icon: const Icon(Icons.shopping_cart,
+                        color: ColorPallete.backgroundColorLight, size: 30),
+                    alignment: Alignment.centerRight,
+                    onPressed: () => _pushCartPage(context),
+                  )
+            )
+          )
+        ],
+      ),
+    );
+  }
+}

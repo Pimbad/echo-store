574bc97eea5fad4c5f6f204408f3031b0e1ee737
eschechola <lucas.eschechola@outlook.com>
2023-05-17 01:43:57 -0300

    feature: add finish order page

---
+++ src/lib/pages/finish.page.dart
@@ -0,0 +1,82 @@
+import 'package:echo_store/pages/home.page.dart';
+import 'package:echo_store/router/router.dart';
+import 'package:echo_store/utils/color.pallete.dart';
+import 'package:echo_store/widgets/echo.button.widget.dart';
+import 'package:echo_store/widgets/echo.page.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:random_string/random_string.dart';
+
+import '../utils/sizes.dart';
+import '../widgets/echo.home.header.widget.dart';
+
+class FinishPage extends StatefulWidget {
+  const FinishPage({super.key});
+
+  @override
+  State<FinishPage> createState() => _FinishPageState();
+}
+
+class _FinishPageState extends State<FinishPage>{
+  final String _orderHash = randomAlpha(12);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void pushOrdersPage() => EchoRouter.push(const HomePage(), context);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: 
+        EchoPage(
+          Center(
+            child: SizedBox(
+              height: Sizes.getPercentHeight(context, 75),
+              width: Sizes.getPercentWidth(context, 85),
+              child: Column(children: [
+                const EchoHomeHeader(
+                  logout: false,
+                  cart: false,
+                  returnPage: HomePage(),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      style: const TextStyle(
+                        color: ColorPallete.backgroundColorLight,
+                        fontSize: 22,
+                      ),
+                      children: [
+                        const TextSpan(text: "Seu pedido \n "),
+                          TextSpan(
+                            text: "#${_orderHash.toUpperCase()} \n",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: ColorPallete.backgroundColor,
+                              fontSize: 26,
+                            )
+                          ),
+                          const TextSpan(text: "foi realizado com sucesso!")
+                        ]
+                      ) 
+                    ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 40),
+                  child: EchoButton(
+                  text: "Ver pedidos",
+                  height: 50,
+                  onPressed: pushOrdersPage) 
+                )
+              ])
+            )
+          )
+        )
+      );
+  }
+}
